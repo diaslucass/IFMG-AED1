17b2f8f17abc7a186d533186378c2559e618b871
Victor <vbhy91zr@duck.com>
2025-06-16 20:39:16 -0300

    Add initial implementation of a C program to calculate and display sum of array elements

---
+++ Tubeto/16/06/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int nums[] = {1, 0, 5, -2, -5, 7};
+    int sum = 0;
+
+    sum = nums[0] + nums[1] + nums[5];
+
+    printf("O valor da soma é: %d\n", sum);
+
+    nums[4] = 100;
+    
+    for (int i = 0; i < 6; i++) {
+        printf("O valor do elemento %d é: %d\n", i, nums[i]);
+    }
+
+    return 0;
+}
