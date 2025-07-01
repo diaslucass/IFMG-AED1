25f2c3346cac861b1077eb0058d7469a384ed527
Victor <vbhy91zr@duck.com>
2025-07-01 11:20:47 -0300

    Added list exercises

---
+++ Tubeto/ListaVetoresMatrizes/2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int v[5];
+    int n = 5;
+    float media = 0, resultado = 0;
+
+    puts("Digite um valor para o vetor: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+        media = media + v[i];
+    }
+
+    media = media / n;
+
+    for (int i = 0; i < n; i++) {
+        resultado = resultado + pow(v[i] - media, 2);
+    }
+
+    resultado = resultado / n;
+    resultado = sqrt(resultado);
+
+    printf("Resultado: %f\n", resultado);
+
+    return 0;
+}
